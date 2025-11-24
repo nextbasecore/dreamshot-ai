b2a2d1bba494f5c9ea4376b1db1bdbd594c8db6b
Harshil Lakhani <harshil@infinitycorp.tech>
2025-11-24 11:24:04 +0530

    update runtime.constants

--- src/constants/runtime.constants.ts
+++ src/constants/runtime.constants.ts
@@ -3,6 +3,9 @@ export const S3_BUCKET_NAME = "clothesswapper";
 
 export const CDN_ENDPOINT = `https://cdn.maxstudio.ai/file/${S3_BUCKET_NAME}`;
 
+// Core API constants
+export const CORE_API_ENDPOINT = "https://api.chromastudio.ai";
+
 // Generation API constants
 export const GENERATION_API_BASE_URL = "https://api.chromastudio.ai";
 export const IMAGE_GEN_ENDPOINT = "/image-gen";
