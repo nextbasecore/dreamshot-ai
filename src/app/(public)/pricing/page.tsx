60c9c34ee09807b72d76b8554d183e762ad835e6
Harshil Lakhani <harshil@infinitycorp.tech>
2025-12-12 11:41:54 +0530

    add footer links and meta data

--- src/app/(public)/pricing/page.tsx
+++ src/app/(public)/pricing/page.tsx
@@ -4,7 +4,16 @@ import FAQs from "@/components/FAQs";
 import CurvedCarousel from "@/components/CurvedCarousel";
 import { getPlansServer } from "@/utils/getPlans.server";
 import { Plan } from "@/types";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+    title: "Affordable pricing & plans | Dreamshot Art",
+    description:
+      "Discover affordable pricing and flexible plans at Dreamshot Art. Get high-quality AI image creation. Explore options that fit your needs. Enjoy creative flow.",
+    alternates: {
+      canonical: "https://www.dreamshot.art/pricing",
+    },
+  };
 
 export default async function PricingPage() {
     let plans: Plan[] | undefined;
