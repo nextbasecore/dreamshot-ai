a87b5481f70401f332727de0c365a68040f6c158
Jay-1410 <jay@infinitycorp.tech>
2025-12-17 17:04:10 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'assets.dreamshot.art',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
